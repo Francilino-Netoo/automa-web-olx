e8d81f2cc201443d8e5c03442ca85ef67160184f
Francilino-Netoo <francilino12345@gmail.com>
2025-03-14 13:08:51 -0300

    ajustes basicos

--- src/components/partial/AdItem/index.js
+++ src/components/partial/AdItem/index.js
@@ -5,17 +5,10 @@ import { Item } from "./styled";
 const AdItem = ({ data }) => {
   if (!data) return null;
 
-  // Verifica e ajusta a URL da imagem, se necessário
-  let imageUrl = data.image;
-  if (
-    imageUrl &&
-    imageUrl.startsWith("https://back-and-api-basica.onrender.com/media/")
-  ) {
-    imageUrl = imageUrl.replace(
-      "https://back-and-api-basica.onrender.com/media/",
-      ""
-    );
-  }
+  // Adiciona a URL completa caso esteja faltando
+  const imageUrl = data.image.startsWith("http")
+    ? data.image
+    : `https://back-and-api-basica.onrender.com/media/${data.image}`;
 
   const price = data.priceNegotiable ? "Preço negociável" : `R$ ${data.price}`;
 
@@ -23,10 +16,7 @@ const AdItem = ({ data }) => {
     <Item>
       <Link to={`/ad/${data.id}`}>
         <div className="itemImage">
-          <img
-            src={imageUrl || "/default-image.jpg"}
-            alt={data.title || "Anúncio"}
-          />
+          <img src={imageUrl} alt={data.title || "Anúncio"} />
         </div>
         <div className="itemName">{data.title || "Sem título"}</div>
         <div className="itemPrice">{price}</div>
